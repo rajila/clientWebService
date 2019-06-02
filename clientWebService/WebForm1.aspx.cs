2d4d0876506b8ebef46212b50a8f0abe31812526
Ronald <rdajila@gmail.com>
2019-06-02 18:50:28 +0200

    Agregar archivos de proyecto.

---
+++ clientWebService/WebForm1.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace clientWebService
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ServiceReferenceOne.ServiceOneClient _service = new ServiceReferenceOne.ServiceOneClient();
+            var _mensaje = _service.getMessage("Ronald");
+            lblMessage.Text = _mensaje.ToString();
+        }
+    }
+}
